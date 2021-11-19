1c8e8d07c944405b1bfbd8249ca97199220d2468
Andrewnomaan <andrew.nomaan@gmail.com>
2021-11-19 17:42:42 +0530

    footer

---
+++ footer.php
@@ -0,0 +1,9 @@
+ <!-- ======= Footer ======= -->
+ <footer id="footer" class="footer">
+        <div class="copyright">
+            &copy; Copyright <strong><span>Zoyo Ecommerce Pvt. Ltd.</span></strong>. All Rights Reserved
+        </div>
+        <div class="credits">
+            Developed by <a href="https://zoyoecommerce.com/">Zoyo Ecommerce Pvt. Ltd.</a>
+        </div>
+    </footer><!-- End Footer -->
