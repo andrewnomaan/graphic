09573c35eef9190d282cfd44098a195371d427e2
Andrewnomaan <andrew.nomaan@gmail.com>
2021-11-22 18:17:40 +0530

    upload_form

--- upload_form.php
+++ upload_form.php
@@ -9,7 +9,7 @@
                               <b> Graphic added successfully.</b>
                         </div>
                         <form action="action.php" method="post" enctype="multipart/form-data" class="graphic_form"
-                            id="form">
+                            id="graphic_form">
                             <div class="row">
                                 <div class="col-lg-8 mb-3">
                                     <input class="form-control graphic" type="file" id="formFile" alt="Image/Video not found" name="graphic_name">
